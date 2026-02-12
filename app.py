852d8000ddbe8664479888bda118113e0572c5da
Sankar Ramamurthy <sankar_ramamurthy@yahoo.co.in>
2026-02-12 10:21:01 +0000

    modification

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('My First Streamlit App in PyCharm')
-st.write('Hello, world! This app is running in PyCharm.')
+st.write('Hello, world! This app is developed in Codespaces.')
