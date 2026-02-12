ccfa542766de09fcd04fc1ce87ee3f4a9d0390b3
SankarRamamurthy <sankar.ramamurthy@ibsplc.com>
2026-02-12 17:57:00 +0530

    Kiro changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('My First Streamlit App')
-st.write('Hello, world! This app is developed in Codespaces.')
+st.write('Hello, world! This app is developed in Kiro.')
